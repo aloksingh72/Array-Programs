f6ecbebdb73d4b81e8efb483f03ce772d35ffe17
Alok Singh <aloksingh36757@gmail.com>
2024-10-20 09:25:18 +0530

    array updated

--- Program42.java
+++ Program42.java
@@ -1,5 +1,12 @@
 public  class Program42{
     public static void main(String[] args) {
+        int[] a ={12,14,16,3,8,9,10};
+        //method calling
+
+
+        
+    }
+    public static void method(int[] a){
         
     }
 }
