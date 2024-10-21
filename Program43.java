f816d52a5987625455dcbff37d24cdef9638fe67
Alok Singh <aloksingh36757@gmail.com>
2024-10-21 11:47:34 +0530

    array updated

--- Program43.java
+++ Program43.java
@@ -1,5 +1,6 @@
 /*Dated-> 20/10/2024/Sunday
 * Reverse Integer
+* Leetcode problem
  * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
  */
 
---
+++ Program44.java
@@ -0,0 +1,11 @@
+public class Program44 {
+    public static void main(String[] args) {
+        int[] a  ={15,16,18,19,20,21,22,23,24,25,78};
+        for(int p:a){
+            System.out.print(p+" ");
+        }
+
+    }
+
+    
+}
