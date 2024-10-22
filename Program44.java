4477b4d8253e9ca2045ce5e064f462a05524fd8c
Alok Singh <aloksingh36757@gmail.com>
2024-10-22 15:36:27 +0530

    array updated

--- Program44.java
+++ Program44.java
@@ -1,6 +1,6 @@
 public class Program44 {
     public static void main(String[] args) {
-        int[] a  ={15,16,18,19,20,21,22,23,24,25,78};
+        int[] a  ={15,16,18,19,20,21,22,23};
         for(int p:a){
             System.out.print(p+" ");
         }
