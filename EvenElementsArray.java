fa56308a228a890571d58c7434140d4b248d8186
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2024-10-28 14:43:50 +0530

    Update EvenElementsArray.java

--- EvenElementsArray.java
+++ EvenElementsArray.java
@@ -3,10 +3,10 @@
 class EvenElementsArray {
 	public static void main(String[] args) {
 		int[] arr = { 12, 23, 45, 56, 23, 8 };
-
+          //method calling 
 		getEvenElements(arr);
 	}
-
+      //method declaration 
 	public static void getEvenElements(int[] arr) {
 
 		for (int i = 0; i < arr.length; i++) {
