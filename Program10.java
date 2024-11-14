2013948e0dc57611a2cc6f8e32d6eb6790c185f6
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2024-11-14 18:53:21 +0530

    Update Program10.java

--- Program10.java
+++ Program10.java
@@ -12,6 +12,7 @@ class Program10{
 			int count =0;
 			for(int i =0;i<s.length;i++){
 				if(s[i].length() %2 ==0){
+					//it will check the length of string divisible by 2 or not
 						System.out.println(s[i].length());
 						count= count+1;	
 						}	
